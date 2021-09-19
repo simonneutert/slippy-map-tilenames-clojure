386fc0843c72c57ee27dfca1cf03179d35f89e09
Simon Neutert <simonneutert@users.noreply.github.com>
2021-09-19 20:54:37 +0200

    cosmetics

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject slippy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "slippy name for gps latitude longitude for a given zoom"
+  :url "https://github.com/simonneutert/slippy-map-tilenames-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
--- src/slippy/core.clj
+++ src/slippy/core.clj
@@ -13,20 +13,20 @@
     (math/expt 2 zoom)
     (/ (- 1 (/ (Math/log (+ (Math/tan (/ (* (float lat) Math/PI) 180)) (/ 1 (Math/cos (/ (* lat Math/PI) 180))))) Math/PI)) 2))))
 
-(defn tile2longitude [x zoom]
+(defn tile2longitude [tilex zoom]
   (float
-   (- (/ (* x 360) (math/expt 2 zoom)) 180)))
+   (- (/ (* tilex 360) (math/expt 2 zoom)) 180)))
 
-(defn tile2latitude [y zoom]
+(defn tile2latitude [tiley zoom]
   (/
    (*
     180
-    (Math/atan (Math/sinh (* Math/PI (- 1 (* 2 (/ y (math/expt 2 zoom))))))))
+    (Math/atan (Math/sinh (* Math/PI (- 1 (* 2 (/ tiley (math/expt 2 zoom))))))))
    Math/PI))
 
-(defn tile2latlng [x y zoom]
-  (let [lat (tile2latitude y zoom)
-        lng (tile2longitude x zoom)]
+(defn tile2latlng [tilex tiley zoom]
+  (let [lat (tile2latitude tiley zoom)
+        lng (tile2longitude tilex zoom)]
     {:lat lat :lng lng :zoom zoom}))
 
 (defn latlng2tile [lat lng zoom]
