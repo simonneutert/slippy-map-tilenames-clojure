cf25906717a3fc2cfcfd383237e12350740d8c60
Simon Neutert <simonneutert@users.noreply.github.com>
2022-08-20 11:59:42 +0200

    drops dependency

--- src/slippy/core.clj
+++ src/slippy/core.clj
@@ -1,27 +1,27 @@
 (ns slippy.core
-  (:require [clojure.math.numeric-tower :as math]))
+  (:require [clojure.math :as math]))
 
 (defn longitude2tile [lon zoom]
   (int
    (*
-    (math/expt 2 zoom)
+    (math/pow 2M zoom)
     (/ (+ (float lon) 180) 360))))
 
 (defn latitude2tile [lat zoom]
   (int
    (*
-    (math/expt 2 zoom)
+    (math/pow 2M zoom)
     (/ (- 1 (/ (Math/log (+ (Math/tan (/ (* (float lat) Math/PI) 180)) (/ 1 (Math/cos (/ (* lat Math/PI) 180))))) Math/PI)) 2))))
 
 (defn tile2longitude [tilex zoom]
   (float
-   (- (/ (* tilex 360) (math/expt 2 zoom)) 180)))
+   (- (/ (* tilex 360) (math/pow 2M zoom)) 180)))
 
 (defn tile2latitude [tiley zoom]
   (/
    (*
     180
-    (Math/atan (Math/sinh (* Math/PI (- 1 (* 2 (/ tiley (math/expt 2 zoom))))))))
+    (Math/atan (Math/sinh (* Math/PI (- 1 (* 2 (/ tiley (math/pow 2M zoom))))))))
    Math/PI))
 
 (defn tile2latlng [tilex tiley zoom]
--- test/slippy/core_test.clj
+++ test/slippy/core_test.clj
@@ -9,6 +9,20 @@
       (latlng2tile 50 8 14)
       {:zoom 14 :tilex 8556 :tiley 5556}))))
 
+(deftest slippy-test-doubles
+  (testing "Slippy Tile for zoom 14 with doubles lat lng"
+    (is
+     (=
+      (latlng2tile 50.1234 8.1234 14)
+      {:zoom 14 :tilex 8561 :tiley 5547}))))
+
+(deftest slippy-test-doubles-16
+  (testing "Slippy Tile for zoom 14 with doubles lat lng"
+    (is
+     (=
+      (latlng2tile 50.1234 8.1234 16)
+      {:zoom 16 :tilex 34246 :tiley 22191}))))
+
 (deftest slippy-test-reverse
   (testing "Lat Lng from slippy zoom 14"
     (is
