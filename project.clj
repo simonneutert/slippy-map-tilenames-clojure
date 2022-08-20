266d19d2df5d97cec58ea5911b36219552e111d5
Simon Neutert <simonneutert@users.noreply.github.com>
2022-08-20 12:02:23 +0200

    bumps version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slippy "0.1.0-SNAPSHOT"
+(defproject slippy "0.1.1-SNAPSHOT"
   :description "slippy name for gps latitude longitude for a given zoom"
   :url "https://github.com/simonneutert/slippy-map-tilenames-clojure"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
